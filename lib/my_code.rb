74a764b7fb178d2390a108590ae402b0e04c19f9
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-10 19:41:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -88,5 +88,11 @@ return true
 end
 
 def reduce_to_any_true(source_array)
+index = 0
+while index < source_array.length do
 
+return true if source_array[index]
+  index += 1
+end
+return false
 end
