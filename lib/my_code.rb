360f4dc0fd5408a01a38f6fa3c5f9e6b77d24bd0
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-10 19:18:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,91 @@
-# My Code here....
+def map_to_negativize(source_array)
+
+new_array = []
+  index = 0
+
+  while index < source_array.length do
+    new_array << source_array[index] * -1
+
+    index += 1
+  end
+
+  new_array
+
+end
+
+def map_to_no_change(source_array)
+
+array_new = []
+index = 0
+
+while index < source_array.length do
+
+array_new << source_array[index]
+
+index += 1
+end
+array_new
+end
+
+def map_to_double(source_array)
+  new_array = []
+index = 0
+while index < source_array.length do
+  new_array << source_array[index] + source_array[index]
+  index += 1
+end
+new_array
+end
+
+
+
+def map_to_square(source_array)
+
+new_array = []
+
+index = 0
+
+while index < source_array.length do
+  num =  source_array[index]
+  new_array << num * num
+  index += 1
+end
+new_array
+end
+
+
+def reduce_to_total(source_array, starting_point = 0)
+
+
+total = starting_point
+
+index = 0
+
+while index < source_array.length do
+
+
+total += source_array[index]
+
+  index += 1
+
+end
+
+total
+end
+
+def reduce_to_all_true(source_array)
+
+ source_array.reduce {|n|
+   if n == 1
+     p true
+   else
+    p false
+   end
+ }
+
+
+end
+
+def reduce_to_any_true(source_array)
+
+end
